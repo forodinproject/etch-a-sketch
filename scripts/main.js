a26575e555ce0b887cc1ca1124eb3b72932fb1cd
forodinproject <112388027+forodinproject@users.noreply.github.com>
2024-03-28 23:06:15 +0530

    Create basic 16x16 grid layout using flexbox

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 let gridSize = 16;
+let heightOfBox = 1024 / gridSize;
+
 const gridWrapper = document.querySelector('.gridWrapper');
-const content = document.createElement('div');
-content.classList.add('contentBox');
-content.style.cssText = "border: 1px solid black; aspect-ratio: 1 / 1; height: 10%";
-for (let i = gridSize; i > 0; i--) {
+
+for (let i = gridSize ** 2; i * i > 0; i--) {
+    const content = document.createElement('div');
+    content.style.cssText = `border: 1px solid black; aspect-ratio: 1 / 1; height: ${heightOfBox}px`;
     gridWrapper.appendChild(content);
 }
