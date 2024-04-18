9ecb8942685b4b7397f329e3a86a717744292e5f
forodinproject <112388027+forodinproject@users.noreply.github.com>
2024-04-18 22:00:49 +0530

    Make the grid smaller

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
 let gridSize = 16;
-let heightOfBox = 1024 / gridSize;
+let heightOfBox = 512 / gridSize;
 
 const gridWrapper = document.querySelector('.gridWrapper');
 
 for (let i = gridSize ** 2; i * i > 0; i--) {
     const content = document.createElement('div');
     content.style.cssText = `border: 1px solid black; aspect-ratio: 1 / 1; height: ${heightOfBox}px`;
+    content.classList.add('changeColor')
     gridWrapper.appendChild(content);
+
+}
+changeColor = document.querySelectorAll('.changeColor')
+
+for (change of changeColor) {
+    change.addEventListener('mouseover', () => {
+        change.style.backgroundColor = 'black';
+    })
 }
